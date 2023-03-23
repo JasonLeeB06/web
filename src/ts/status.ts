7e498d49ce47dfdf0db88be44b0251db74229907
Jason B <82266978+JasonLeeB06@users.noreply.github.com>
2023-03-23 09:52:34 +0100

    file rename

--- src/ts/status.ts
+++ src/ts/status.ts
@@ -7,7 +7,7 @@ const timeAgo = new TimeAgo('en-US');
 
 $(document).ready(() => {
     $.get("https://koyu.space/api/v1/accounts/109642905885595484/statuses?exclude_replies=true&exclude_reblogs=true", (data) => {
-        $("#status-content").html(data[0].content);
-        $("#status-time").html(`<a href="${data[0].url}" target="_blank">${timeAgo.format(new Date(data[0].created_at))}</a>`);
+        $("#mstdn-status-content").html(data[0].content);
+        $("#mstdn-status-time").html(`<a href="${data[0].url}" target="_blank">${timeAgo.format(new Date(data[0].created_at))}</a>`);
     });
 });
