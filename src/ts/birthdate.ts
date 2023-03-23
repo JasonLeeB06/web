52626a325e8e2d92f317511234e6dd82079f5dc8
Jason B <82266978+JasonLeeB06@users.noreply.github.com>
2023-03-23 09:51:50 +0100

    added birthdate counter

---
+++ src/ts/birthdate.ts
@@ -0,0 +1,27 @@
+import $ from "jquery";
+
+$(document).ready(function() {
+    var endDate = new Date("Feb 20, 2024 04:29:00").getTime();
+  
+    var countdownInterval = setInterval(function() {
+      var now = new Date().getTime();
+    
+      var distance = endDate - now;
+      
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      
+      $('#birthdate-countdown').html(days + " Days, " + hours + " Hours, " + minutes + " Minutes, " + seconds + " Seconds");
+      
+      if (distance < 0) {
+        clearInterval(countdownInterval);
+        $('#birthdate-countdown').html("Happy Birthday!");
+      }
+
+      if (new Date().getFullYear() > new Date(endDate).getFullYear()) {
+        endDate = new Date("Feb 20, " + (new Date().getFullYear() + 1) + " 00:00:00").getTime();
+      }
+    }, 1000);
+});
