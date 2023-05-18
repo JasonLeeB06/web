b84fbc633111ca216e9611d129a313b47355c947
Jason B <82266978+JasonLeeB06@users.noreply.github.com>
2023-05-18 19:35:18 +0200

    Use cutebox

--- src/ts/status.ts
+++ src/ts/status.ts
@@ -6,7 +6,7 @@ TimeAgo.addDefaultLocale(en);
 const timeAgo = new TimeAgo('en-US');
 
 $(document).ready(() => {
-    $.get("https://koyu.space/api/v1/accounts/109642905885595484/statuses?exclude_replies=true&exclude_reblogs=true", (data) => {
+    $.get("https://cutebox.cc/api/v1/accounts/01B9Y4T2JKA467BMXGQT043HHK/statuses?exclude_replies=true&exclude_reblogs=true", (data) => {
         $("#status-content").html(data[0].content);
         $("#status-time").html(`<a href="${data[0].url}" target="_blank">${timeAgo.format(new Date(data[0].created_at))}</a>`);
     });
