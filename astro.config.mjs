f1e1ece3a98fd6efa58cce325a2981714b755558
Jason B. <jasonleebaerwald@gmail.com>
2024-08-26 19:34:45 +0200

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config';
-import icon from "astro-icon";
+
 import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), icon({
-    include: {
-      mdi: ["*"], // (Default) Loads entire Material Design Icon set
-      mdi: ["account"], // Loads only Material Design Icon's "account" SVG
-    },
-  })],
+  integrations: [mdx(), icon()],
 });
