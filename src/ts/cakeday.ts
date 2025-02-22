a36e3328808560582a51ff35130cfc8ac2e10d50
Jason B. <jasonleebaerwald@gmail.com>
2025-02-22 22:35:38 +0100

    Update cakeday.ts

--- src/ts/cakeday.ts
+++ src/ts/cakeday.ts
@@ -1,27 +1,36 @@
 import $ from "jquery";
 
-$(document).ready(function() {
-     var endDate = new Date("Feb 20, 2025 04:29:00").getTime();
-  
-      var countdownInterval = setInterval(function() {
-      var now = new Date().getTime();
+$(document).ready(function () {
+  function getNextEndDate() {
+    let now = new Date();
+    let year = now.getFullYear();
+    let endDate = new Date(`Feb 20, ${year} 04:29:00`).getTime();
     
-      var distance = endDate - now;
-      
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      
-      $('#cakeday-countdown').html(days + " Days, " + hours + " Hours, " + minutes + " Minutes, " + seconds + " Seconds");
-      
-      if (distance < 0) {
-        clearInterval(countdownInterval);
-        $('#cakeday-countdown').html("Happy Birthday!");
-      }
+    if (now.getTime() > endDate) {
+      endDate = new Date(`Feb 20, ${year + 1} 04:29:00`).getTime();
+    }
+    
+    return endDate;
+  }
 
-      if (new Date().getFullYear() > new Date(endDate).getFullYear()) {
-        endDate = new Date("Feb 20, " + (new Date().getFullYear() + 2) + " 04:29:00").getTime();
-      }
-    }, 1000);
-})
+  let endDate = getNextEndDate();
+
+  let countdownInterval = setInterval(function () {
+    let now = new Date().getTime();
+    let distance = endDate - now;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    if (distance < 0) {
+      endDate = getNextEndDate();
+      return;
+    }
+
+    $("#cakeday-countdown").html(
+      days + " Days, " + hours + " Hours, " + minutes + " Minutes, " + seconds + " Seconds"
+    );
+  }, 1000);
+});
